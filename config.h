1a225b3056753231822eeb4c28319782e4fd9d9a
amitshky <amitshkymail@gmail.com>
2025-11-27 13:19:18 +0000

    font size and border size changed

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=19:antialias=true:autohint=true";
-static int borderpx = 1;
+static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
